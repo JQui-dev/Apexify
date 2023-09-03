e654139a38cf7096a0374b99b97033c9a48af9d4
Q1one <jqui.dev@gmail.com>
2023-09-02 20:53:20 -0500

    Landapage previsualizer

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom'
+
+import './Main.scss'
+
 function Main () {
   return (
-    <div>
-      <h1>Main</h1>
+    <div className='Main'>
+      {/* Div just avoid the gap */}
+      <div>
+        <h1>GP TRACKER</h1>
+        <h2>Follow your favorite drivers and teams</h2>
+      </div>
+
+      <Link to='/'>Coming Race</Link>
+
+      <img src='/car.png' alt='car' />
     </div>
   )
 }
