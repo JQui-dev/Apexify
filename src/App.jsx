42a3c761119da1953a964a7c95ccbd1b5f9b2d7d
Q1one <jqui.dev@gmail.com>
2023-09-02 19:33:51 -0500

    NavBar & mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // PAGES
-import Land from './pages/Land'
+import Main from './pages/Main'
 import Error from './pages/Error'
-import Next from './pages/Next'
-import Last from './pages/Last'
-import Stand from './pages/Stand'
-import Profile from './pages/Profile'
 
 // COMPONENTS
 import NavBar from './components/NavBar'
 
 // STYLE
-import './style/App.scss'
+import './App.scss'
 
 function App () {
   return (
     <div className='App'>
       <BrowserRouter>
         <NavBar />
-        <Routes>
-          <Route path='/' element={<Land />} />
-          <Route path='*' element={<Error />} />
-
-          <Route path='/next' element={<Next />} />
-          <Route path='/last' element={<Last />} />
-          <Route path='/stand' element={<Stand />} />
-          <Route path='/profile' element={<Profile />} />
-        </Routes>
+        <div className='screen'>
+          <Routes>
+            <Route path='/' element={<Main />} />
+            <Route path='*' element={<Error />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   )
